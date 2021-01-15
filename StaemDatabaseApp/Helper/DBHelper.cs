e9d5c10c303c7eb23674db3f4f417daface5bf9d
Michał Tkacz <58853722+michaltkacz@users.noreply.github.com>
2021-01-15 13:17:59 +0100

    Add project files.

---
+++ StaemDatabaseApp/Helper/DBHelper.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StaemDatabaseApp.Helper
+{
+    public static class DBHelper
+    {
+        private static MySqlConnection connection;
+        private static MySqlCommand cmd = null;
+        private static DataTable dt;
+        private static MySqlDataAdapter sda;
+
+        public static void EstablishConnection()
+        {
+            try
+            {
+                MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+                builder.Server = "127.0.0.1";
+                builder.UserID = "root";
+                builder.Password = "root";
+                builder.Database = "staem";
+                builder.SslMode = MySqlSslMode.None;
+                connection = new MySqlConnection(builder.ToString());
+                connection.Open();
+                MessageBox.Show("Database connection successfull", "Connection", MessageBoxButton.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Connection", MessageBoxButton.OK);
+            }
+        }
+
+    }
+}
