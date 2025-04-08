70ad65ac32a957fbae1b8cb5d3a7a731e0126bbb
Ярослав Хиля <Yakhilya@gmail.com>
2025-04-09 00:55:22 +0300

    add Object cs

---
+++ StaemDatabaseApp/Model/Object.cs
@@ -0,0 +1,42 @@
+﻿using StaemDatabaseApp.DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaemDatabaseApp.Model
+{
+    public class Object
+    {
+
+        public Object(string id, string clientID, string type, string address)
+        
+            int id_ = 0;
+            Int32.TryParse(id, out id_);
+
+            Id = id_;
+            ClientID = clientID;
+            Type = type;
+            Address = address;
+        }
+
+        private int id;
+        private int clientID;
+        private string address;
+        private string type;
+
+        public string Type { get => type; set => type = value; }
+        public int ClientID { get => clientID; set => clientID = value; }
+        public int Id { get => id; set => id = value; }
+        public string Address { get => address; set => address = value; }
+
+        public override string ToString()
+    {
+            return address + " : " + Id.ToString();
+        }
+
+    }
+}
