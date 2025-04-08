c82df16508c19a9ca4d1bd36e3d1285389a9309e
Ярослав Хиля <Yakhilya@gmail.com>
2025-04-09 00:49:26 +0300

    add  Employees cs

---
+++ StaemDatabaseApp/Model/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaemDatabaseApp.Model
+{
+    public class Employee
+    {
+
+        public Employee(string id, string fullName, int positionID, string phone, string email, string login, string password);
+        {
+            int id_ = 0;
+            Int32.TryParse(id, out id_);
+
+            Id = id_;
+            FullName = fullName;
+            PositionID = positionID;
+            Phone = phone;
+            Email = email;
+            Login = login;
+            Password = password;
+        }
+
+        private int id;
+        private string fullName;
+        private int positionID;
+        private string phone;
+        private string email;
+        private string login;
+        private string password;
+
+
+
+        public string Phone { get => phone; set => phone = value; }
+        public string Email { get => email; set => email = value; }
+        public string FullName { get => fullName; set => fullName = value; }
+        public int PositionID { get => positionID; set => positionID = value; }
+        public int Id { get => id; set => id = value; }
+        public string login { get => login; set => login = value; }
+        public string Password { get => password; set => password = value; }
+
+    public override string ToString()
+        {
+            return fullName + " : " + Id.ToString();
+        }
+
+    }
+}
